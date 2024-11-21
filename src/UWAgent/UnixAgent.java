910b5bb47fc68a849dfb5e111fda2409e20bea9a
MaryamM4 <maryamma@outlook.com>
2024-11-21 12:34:21 -0800

    UnixAgent argument parsing (taken from UnixClient and modified).
    Needs double checking

--- src/UWAgent/UnixAgent.java
+++ src/UWAgent/UnixAgent.java
@@ -17,18 +17,69 @@ public class UnixAgent extends UWAgent implements Serializable {
         System.out.println("No arguments.");
     }
 
-    public UnixAgent(String[] args) {
-        // Set the print variable
-        // Init the itinerary server[]
-        // Init commands[]
+    /**
+     * 
+     * @param args [0] P or C option, 
+     *             [1] Number of servers, [2] List of server IP names, 
+     *             [3] Number of Unix commands, [4] List of Unix commands.
+     * 
+     * @throws IllegalArgumentException On invalid parameters or failed initialization. 
+     */
+    public UnixAgent(String[] args) throws IllegalArgumentException {
+        int argc = args.length;
+
+        // Check minimum size
+        if (argc < 6) {
+            throw new IllegalArgumentException("UnixAgent Error: Must have 5 params minimum: option, numServers, serverName/s, numCommands, command/s.");
+        }
+
+        // Set the print variable:
+        // If P, print all command outputs at the end
+        // Otherwise, print only the number of line recieved by the servers.    
+        print = args[0].equals("P");
+     
+        // Init the itinerary server[]: 
+        int numServers = Integer.parseInt(args[1]);
+        if (numServers < 1 || numServers > argc - 4) {
+            throw new IllegalArgumentException("UnixAgent Error: Paramater numServers " + numServers + " cannot be < 1 or exceed param count.");
+        } 
+
+        servers = new String[numServers];
+        for (int i = 0; i < numServers; i++) {
+            servers[i] = args[i + 2];
+        }
+
+        // Init commands[]:
+        int numCommands = Integer.parseInt(args[1+numServers]);
+        if (numCommands < 1 || numCommands > argc - (4 + numServers)) {
+            throw new IllegalArgumentException("UnixAgent Error: Paramater numCommands " + numCommands + " cannot be < 1 or exceed param count.");
+        } 
+
+        commands = new String[numCommands];
+        for (int i = 0; i < numCommands; i++) {
+            commands[i] = args[i + 3 + numServers];
+        }
+
+        // Display outcome
+        String option = "count";
+        if (print) {
+            option = "print";
+        }
+
+        System.out.println("print/count = " + option + ", nServers = " + numServers + ", server1 = " + servers[0] + ", command1 = " + commands[0]);
     }
 
     public void init() {
         startTime = new Date(); 
 
-        // Memorize where this agent was injected
-        InetAddress inetaddr = InetAddress.getLocalHost(); 
-        orgPlace = inetaddr.getHostAddress();   // Hostname
+        try { // Memorize where this agent was injected
+            InetAddress inetaddr = InetAddress.getLocalHost(); 
+            orgPlace = inetaddr.getHostAddress();   // Hostname
+
+        } catch (UnknownHostException hostExc) {
+            System.out.println("UnixAgent failed to get local host address: ");
+            hostExc.printStackTrace(System.out);
+        }
 
         // Go to servers[0] and call unix()
         //hop(orgPlace, "result", ...);
